cd2bf88ccb1879ac807eeb7c3aebaf5034a1b54a
Jason <yhorg@hotmail.com>
2018-05-04 13:39:13 +0800

    Minor fix.

--- primer-answer/chapter6/6.46.h
+++ primer-answer/chapter6/6.46.h
@@ -3,6 +3,7 @@
 
 using namespace std;
 
-inline constexpr bool isShorter(const string &s1, const string &s2) {
+// "inline constexpr" = "constexpr" [Default inline style]
+constexpr bool isShorter(const string &s1, const string &s2) {
   return s1.size() < s2.size();
 }
